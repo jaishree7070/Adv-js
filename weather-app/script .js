295eca29b6efd46a52703a6f80f0b48ee03c398f
R Jaishree <109673030+jaishree7070@users.noreply.github.com>
2023-01-20 12:51:12 +0530

    initial commit

---
+++ weather-app/script .js
@@ -0,0 +1,40 @@
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273;
+
+window.addEventListener("load", () => {
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition((position) => {
+	console.log(position);
+	lon = position.coords.longitude;
+	lat = position.coords.latitude;
+
+	// API ID
+	const api = "48e653ec2cc48f1c6082d474f157b3cb";
+
+	// API URL
+	const base =
+`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+`lon=${lon}&appid=${api}`;
+	// Calling API
+	fetch(base)
+		.then((response) => {
+		return response.json();
+		})
+		.then((data) => {
+		console.log(data);
+		temperature.textContent =
+		Math.floor(data.main.temp - kelvin) + "°C";
+		summary.textContent = data.weather[0].description;
+		loc.textContent = data.name + "," + data.sys.country;
+		let icon1 = data.weather[0].icon;
+		icon.innerHTML =
+			`<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
+		});
+	});
+}
+});
